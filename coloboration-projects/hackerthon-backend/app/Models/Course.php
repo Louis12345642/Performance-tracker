42fe7029bd09a91e47af6d89daa5c4dada071fe3
mubarak <luismubarak@gmail.com>
2024-12-30 21:22:50 +0200

    create the relationship between users and course

--- coloboration-projects/hackerthon-backend/app/Models/Course.php
+++ coloboration-projects/hackerthon-backend/app/Models/Course.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
+
+
     /** @use HasFactory<\Database\Factories\CourseFactory> */
     use HasFactory;
+
+    protected $fillable = ['title','catOne','fat','catTwo','Fat','total'];
+
+    //create the relationship between the user and courses
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
 }
